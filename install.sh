9940e7d66e2978e9c8856916831224f4be4e2889
Muhammad Iqbal <76495374+miqbal-22@users.noreply.github.com>
2021-01-18 18:31:55 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install mc
+pkg install tesseract
+pkg install nodejs
+pkg install wget
+pkg install ffmpeg
+clear
+npm i -g ytdl
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i got
+npm i
+clear
+echo "[*]udah selesai nih. Tinggal ketik node index.js"
