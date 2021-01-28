16d6911806e10afb4d53c9afa187147541172ec0
Muhammad Iqbal <76495374+miqbal-22@users.noreply.github.com>
2021-01-28 14:19:18 +0700

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'JINGAN BOT',
+        ownerName: 'Muhammad Iqbal',
+        youtube: 'https://m.youtube.com/channel/UCb6qlya4nhSYqNBdSQVes-A',
+        instagram: 'https://instagram.com/_miqbal.22',
+}
