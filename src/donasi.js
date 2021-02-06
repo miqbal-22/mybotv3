69c445c5787880e0a11a306b56b16713f8015a7d
Muhammad Iqbal <76495374+miqbal-22@users.noreply.github.com>
2021-02-06 18:36:44 +0700

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,13 @@
+const donasi = () => { 
+	return `Mau donasi agar bot bisa terus berjalan.
+
+ فَاتَّقُوا النَّارَ وَلَوْ بِشِقِّ تَمْرَةٍ فَمَنْ لَمْ يَجِدْ فَبِكَلِمَةٍ طَيِّبَةٍ
+_“jauhilah api neraka, walau hanya dengan bersedekah setengah biji kurma atau satu biji kurma(sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
+
+*Pulsa :* _085781406716_
+*Dana :* _085781406716_
+*OVO :* _085781406716_
+*Gopay* _085781406716_`
+}
+
+exports.donasi = donasi
