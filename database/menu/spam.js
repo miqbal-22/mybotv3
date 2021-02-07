99b044e777a09c88b6d3207cc098dd5c83b5679f
Muhammad Iqbal <76495374+miqbal-22@users.noreply.github.com>
2021-02-07 17:04:18 +0700

    Add files via upload

---
+++ database/menu/spam.js
@@ -0,0 +1,36 @@
+const spam = (prefix, botName, ownerName) => {
+        return `
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 「  2.4.9  」
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 SPAM 〉*
+   ╽
+   ┠≽ *${prefix}spamcall* 
+   ┠≽ *${prefix}spamsms*
+   ┠≽ *${prefix}spamgmail*
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
+}
+exports.spam = spam
