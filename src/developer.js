1c70d3c5454bc2aad994d591fcf7215ce251c9c4
Muhammad Iqbal <76495374+miqbal-22@users.noreply.github.com>
2021-02-22 15:01:36 +0700

    Add files via upload

---
+++ src/developer.js
@@ -0,0 +1,38 @@
+const developer = () => {
+	return `
+╔═══════════════════════
+║ *About JINGAN BOT*
+╠═══════════════════════
+║├≽️⚜ *AUTHOR*: _Muhammad Iqbal_
+║├≽️⚜ *DESIGNER*: _Muhammad Iqbal_
+║├≽️⚜ *DONASI*: _085781406716_
+╠═══════════════════════
+║  👾 *INGFO JINGAN BOT* 👾
+║
+║╭──❉ *SOSMED ADMIN* ❉─────
+║│1. *Group Official ${botName)*
+║│ 'https://chat.whatsapp.com/DayC8HtsPGoFXutq9Vk2iq'
+║│
+║│2. *Twitter* <Follow>
+║│ 'https://twitter.com/_miqbal22_'
+║│
+║│2. *YouTube <Subscribe>*
+║│ 'https://youtube.com/channel/UCb6qlya4nhSYqNBdSQVes-A'
+║│
+║│3. *Instagram <Follow>*
+║│ 'https://instagram.com/_miqbal.22 <IG Owner>'
+║│ 'https://instagram.com/unfaedah52 <IG Group>'
+║│ 'https://instagram.com/tontonan.62 <IG Group>'
+║│
+║│4. *Creator/Owner*
+║│ 'https://wa.me/6285745376798'
+║╰──────────────────────
+║
+║       ▌│█║▌║▌║║▌║▌║█│▌
+║       ▌│█║▌║▌║║▌║▌║█│▌
+║
+║ *MADE BY MUHAMMAD IQBAL*
+╚═══════════════════════`
+}
+
+exports.developer = developer
---
+++ src/listzodiak.js
@@ -0,0 +1,120 @@
+function listzodiak() {
+    return `
+╔════「 *ZODIAK LIST* 」
+╠≽ Aries (21 Maret – 20 April)
+╠≽ Taurus (21 April – 20 Mei)
+╠≽ Gemini (21 Mei – 20 Juni)
+╠≽ Cancer (21 Juni – 20 Juli)
+╠≽ Leo (21 Juli – 21 Agustus)
+╠≽ Virgo (22 Agustus – 22 September)
+╠≽ Libra (23 September – 22 Oktober)
+╠≽ Scorpio (23 Oktober – 22 November)
+╠≽ Sagittarius (23 November – 20 Desember)
+╠≽ Capricorn (21 Desember – 19 Januari)
+╠≽ Aquarius (20 Januari – 18 Februari)
+╠≽ Pisces (19 Februari – 20 Maret)
+╚════ #listzodiak *「 B O T 」* `
+}
+exports.listzodiak = listzodiak
+const aries = () => { 
+	return `       
+Aries (21 Maret – 20 April)
+
+Aries (♈) /ˈɛəriːz/ (yang berarti "Domba") adalah rasi Zodiak pertama, yang mencakup 30 derajat pertama bujur langit (0°≤ λ <30°). Di bawah zodiak tropis, Matahari transit di rasi ini umumnya antara 20 Maret sampai 20 April setiap tahunnya. Durasi waktu ini persis bulan pertama Kalender Persia (Farvardin). Di bawah zodiak sidereal, matahari saat transit Aries dari 15 April - 15 Mei (kurang-lebih). Simbol domba yang didasarkan pada Chrysomallus, domba terbang.
+Tergantung pada sistem yang digunakan, individu yang lahir pada tanggal tersebut, dapat disebut Arian atau Ariens . Aries adalah rasi api pertama di zodiak, rasi api lainnya adalah Leo dan Sagitarius
+`
+}
+exports.aries = aries
+const taurus = () => { 
+	return `       
+Taurus (21 April – 20 Mei)
+
+Taurus (Bahasa Latin untuk "Banteng"; simbol:, Unicode: ♉) adalah tanda astrologi kedua di zodiak. Zodiak Ini mencakup tingkat ke 30-60 zodiak, antara 27,25 dan 54,75 derajat bujur langit. Di bawah zodiak tropikal, matahari transit daerah ini pada 20 April sampai 20 Mei setiap tahun. Di bawah zodiak sidereal, matahari transit konstelasi Taurus dari tanggal 16 Mei Sampai 21 Juni. Orang yang lahir antara tanggal ini, tergantung pada sistem astrologi yang mereka pilih, dapat disebut Taureans.[1][2] Simbol banteng didasarkan pada banteng Kreta, seekor banteng putih, ayah Minotaur yang dibunuh oleh Theseus.
+`
+}
+exports.taurus = taurus
+const gemini = () => { 
+	return `       
+Gemini (21 Mei – 20 Juni)
+
+Gemini (♊)[1] adalah zodiak ketiga yang berasal dari konstelasi Gemini. Di bawah zodiak tropis, matahari transit tanda ini antara 21 Mei dan 21 Juni. Gemini diwakili oleh si Kembar Castor dan Pollux.[2] simbol kembar ini didasarkan pada Dioscuri, dua manusia yang diberikan bersama kekuatan dewa setelah kematian mereka.
+`
+}
+exports.gemini =gemini
+const cancer = () => { 
+	return `       
+Cancer (21 Juni – 20 Juli)
+
+Kanser (♋) (Yunani Kuno: Καρκίνος, Latin: Cancer) adalah zodiak keempat yang berasal dari konstelasi Cancer. Zodiak ini mencakup 90° 120° dari zodiak, antara 90° dan 120° dari tata koordinat langit. Di bawah zodiak tropis, Matahari transit daerah ini antara 22 Juni sampai 22 Juli, dan di bawah sideris, Matahari transit daerah ini dari tanggal 16 Juli sampai 15 Agustus.[1]
+Dalam astrologi, Kanser adalah zodiak kardinal dari elemen air, yang terdiri dari Kanser, Pises, dan Skorpio.[2] Zodiak ini berenergi negatif, dan berdomisil di planet Bulan. Kanser berbentuk kepiting, berdasarkan Karkinos, kepiting raksasa yang dilecehkan Heracles selama pertarungannya dengan Hydra.
+`
+}
+exports.cancer = cancer
+const Leo = () => { 
+	return `       
+Leo (21 Juli – 21 Agustus)
+
+Leo (♌) (Yunani Kuno: Λέων, Latin: Leōn), adalah tanda zodiak ke 5, berasal dari konstelasi Leo. Muncul setelah Kanser (Yunani: Καρκίνος, Latin: Karkinos) dan sebelum Virgo (Yunani: Παρθένος, Latin: Parthenos). Pada zodiak tropis, Matahari transit konstelasi ini pada 23 Juli sampai 22 Agustus;
+`
+}
+exports.Leo = Leo
+const Virgo = () => { 
+	return `       
+Virgo (22 Agustus – 22 September)
+
+Virgo (♍) (Yunani: Παρθένος, Parthenos) adalah tanda astrologi keenam dalam Zodiak. Tanda ini mencakup 150-180 derajat zodiak. Di bawah zodiak tropis, Matahari transit area ini rata-rata antara 23 Agustus dan 22 September,[1] dan Matahari transit rasi bintang Virgo dari sekitar 16 September hingga 30 Oktober.[
+`
+}
+exports.Virgo = Virgo
+const Libra = () => { 
+	return `       
+Libra (23 September – 22 Oktober)
+
+Libra (♎) adalah zodiak ketujuh. Zodiak ini mencakup 180°–210° garis bujur.[1] Pada zodiak tropis, matahari transit zodiak ini pada tanggal 23 September dan 23 Oktober,[2][3] dan pada zodiak sideris, matahari berada di konstelasi ini dari 31 Oktober hingga 22 November. [4] Simbol timbangan pada zodiak ini didasari dari Scales of Justice yang dipegang oleh Themis, penjelmaan hukum dari Yunani.[5] Dia menjadi inspirasi modern wanita hukum. Planet yang menaungi Libra adalah Venus.[6][7] Libra adalah zodiak satu-satunya yang digambarkan dengan objek yang tidak hidup. Zodiak lainnya digambarkan dengan hewan atau makhluk mitologi.
+`
+}
+exports.Libra = Libra
+const Scorpio = () => { 
+	return `       
+Scorpio (23 Oktober – 22 November)
+
+Skorpio (♏) (bahasa Yunani: Σκορπιός Skorpios; bahasa Latin: Scorpius) adalah bintang kedelapan dari daftar zodiak. Bintang ini berada diurutan 210-240 zodiak, antara 207,25 dan 234,75 derajat bujur langit. Di bawah zodiak tropis, matahari transit daerah ini rata-rata antara 23 Oktober sampai 22 November, dan di bawah zodiak sidereal, matahari saat transit konstelasi Scorpius dari sekitar November 16 sampai 15 Desember. Tergantung pada sistem zodiak yang digunakan, sebuah individu yang lahir di bawah pengaruh Skorpio dapat disebut Scorpio atau Scorpion.[1] Simbol kalajengking didasarkan pada Scorpius, kalajengking raksasa yang dikirim oleh Gaia untuk membunuh Orion.[2]
+Skorpio adalah zodiak kedua dalam elemen air, setelah Cancer dan diikuti oleh zodiak berelemen air ketiga, Pisces.
+`
+}
+exports.Scorpio = Scorpio
+const Sagittarius = () => { 
+	return `       
+Sagittarius (23 November – 20 Desember)
+
+Sagitarius (♐) adalah zodiak kesembilan dalam urutan di tabel zodiak. Di langit, zodiak ini membentang di antara 240 derajat dan 269 derajat dari sistem koordinat ekliptika. Dalam zodiak tropis, Matahari transit di daerah ini dari 23 November sampai 21 Desember setiap tahun. Dalam astrologi sideris, saat matahari transit di rasi bintang Sagitarius dari sekitar 16 Desember- 14 Januari.
+`
+}
+exports.Sagittarius = Sagittarius
+const Capricorn = () => { 
+	return `       
+Capricorn (21 Desember – 19 Januari)
+
+Kaprikornus adalah salah satu dari rasi bintang zodiak. Biasanya dikenal sebagai Capricorn, khususnya dalam astrologi. Rasi ini melambangkan kambing bertanduk, sekalipun kadang banyak yang menyebutnya kambing laut. Kaprikornus adalah salah satu dari 88 rasi bintang modern, dan juga satu dari 48 rasi bintang yang didaftar oleh Ptolemy. Dalam batas rasi bintang modern, rasi ini dikelilingi oleh Akuila, Sagitarius, Mikroskopium, Piscis Austrinus dan Aquarius.
+Dalam Zodiak, individu yang memiliki bintang Kaprikornus, terlahir pada 22 Desember hingga 19 januari, ketika Matahari ada pada bintang Kapricornus.
+`
+}
+exports.Capricorn = Capricorn
+const Aquarius = () => { 
+	return `       
+Aquarius (20 Januari – 18 Februari)
+
+Akuarius (♒) adalah zodiak kesebelas dari dua belas tanda-tanda zodiak astrologi dan itu berasal dari konstelasi yang mempunyai nama yang sama, yaitu konstelasi Akuarius.
+Individu yang lahir ketika matahari berada di tanda ini dikenal sebagai "individu Akuarius". Akuarius dianggap sebagai zodiak maskulin oleh astrolog kuno dan itu dijajah oleh planet Saturnus, tetapi planet Uranus dikatakan merupakan penjaga nyata Akuarius oleh sejumlah ahli astrologi modern. Nama Sanskerta bagi Akuarius dalam astrologi Hindu ialah Kuṃbha.
+`
+}
+exports.Aquarius = Aquarius
+const Pisces = () => { 
+	return `       
+Pisces (19 Februari – 20 Maret)
+
+Pises (Ikan) adalah suatu zodiak rasi bintang berada antara Akuarius di sebelah barat dan Aries di sebelah timur. Pises merupakan lambang astrologi ke-12 dalam sebuah zodiak, yang berasal dari Konstelasi Pises. Dalam astrologi, Pises identik dengan lambang feminin atau negatif. Juga merupakan perlambangan air. Dalam zodiac dilambangkan dengan sepasang ikan yang berenang dengan arah berlawanan. Secara tradisional, Pises bertahta di Planet Jupiter, tetapi sejak penemuannya, Neptunus merupakan tahta modern dari lambang ini.
+Dalam Zodiak, individu yang memiliki bintang Pises, terlahir pada 19 Februari hingga 20 Maret, ketika Matahari ada pada bintang Pisces.
+`
+}
+exports.Pisces = Pisces
